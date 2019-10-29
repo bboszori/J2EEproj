3605c902d4f4b5564df11b7e01f26ee30cb6dc93
javaee <javaee@javaee-VirtualBox>
2019-10-30 00:44:11 +0100

    Adding dependencies

---
+++ adatok/src/main/java/hu/oe/hoe/adatok/Building.java
@@ -0,0 +1,28 @@
+
+package hu.oe.hoe.adatok;
+
+public class Building {
+    private String name, description;
+
+    public Building(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
